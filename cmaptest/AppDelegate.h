5bd0504c7ac2ca9160d490a0e6fa327c307fef69
AppleUser <appleuser@appbookair.local>
2016-03-27 02:30:52 +0900

    Initial Commit

---
+++ cmaptest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cmaptest
+//
+//  Created by AppleUser on 2016/03/27.
+//  Copyright © 2016年 rizworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cmaptest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cmaptest
+//
+//  Created by AppleUser on 2016/03/27.
+//  Copyright © 2016年 rizworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
